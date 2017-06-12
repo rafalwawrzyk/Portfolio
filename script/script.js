1a4c452fa291020a7472640069b6ac808ce114fd
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2017-06-12 18:06:19 +0200

    add scripts

---
+++ script/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(){
+    
+ 
+        window.onload = function(){
+            var fullpage = document.querySelector('.fullpage');
+            
+        }
+        
+    
+    var hamburger = document.querySelector('.hamburger span');
+    
+    hamburger.addEventListener('click', function(){
+        var nav = document.querySelector('.nav');
+        nav.classList.toggle('visible');
+    },false)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+})
