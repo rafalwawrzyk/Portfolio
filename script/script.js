5cc83e9277b52421376464fc635cd63609aff40e
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2017-06-23 15:26:25 +0200

    script to contact icons

--- script/script.js
+++ script/script.js
@@ -14,10 +14,35 @@ document.addEventListener("DOMContentLoaded", function(){
         nav.classList.toggle('visible');
     },false)
     
+    ///////////////// icon events
+    var phoneIcon = document.querySelector('.phone-icon');
+    var messageIcon = document.querySelectorAll('.modal-icon');
+    var message = document.querySelector('.answer-icon-message');
     
+  
+   for(var i=0; i<messageIcon.length; i++){
+       messageIcon[i].addEventListener('click',function(event){
+           if(event.target.classList.contains('phone-ico')){
+               message.classList.toggle('visible-box');
+                   message.innerHTML = "505-246-348";
+               event.stopPropagation();
+           }else{
+                message.classList.toggle('visible-box');
+               message.innerHTML = "rafal.wawrzyk@gmail.com";
+               event.stopPropagation();
+           }
+           
+       })
+   }
     
+    document.body.addEventListener('click',function(e){
+       message.classList.remove('visible-box');
+    })
     
-    
+//    messageIcon.addEventListener('click', function(){
+//        message.classList.toggle('visible-box');
+//        message.innerHTML = "rafal.wawrzyk@gmail.com"
+//    })
     
     
     
