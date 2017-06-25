72bf8d864b2ef38069b08fb528244374fc3fe491
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2017-06-25 14:02:12 +0200

    animation

--- script/scriptAnchor.js
+++ script/scriptAnchor.js
@@ -3,7 +3,7 @@ $(function(){
     $(document).on('click', 'a', function(event){
         event.preventDefault();
         $('html, body').animate({
-            scrollTop:$($.attr(this, 'href')).offset().top
+            scrollTop:$($.attr(this, 'href')).offset().top - 50
         },500)
     })
     
